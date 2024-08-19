c99120322818872457bd9c3ec2630609f01a0c0d
matilda002 <matilda0817@hotmail.com>
2024-08-19 12:04:51 +0200

    contact form added
    successfully moved from old project

--- roboApp/src/components/Privacy.jsx
+++ roboApp/src/components/Privacy.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import Header from "../components/Header";
+import ContactForm from "../components/ContactForm";
 import { Link } from "react-router-dom";
 
 const Privacy = () => {
-    return (
-        <ul>
-            <li><Link to="/">Homepage</Link></li>
-        </ul>
-    );
+  return (
+    <ul>
+      <div>
+        <Header />
+      </div>
+      <li>
+        <Link to="/">Homepage</Link>
+      </li>
+      <div>
+        <ContactForm />
+      </div>
+    </ul>
+  );
 };
 
 export default Privacy;
