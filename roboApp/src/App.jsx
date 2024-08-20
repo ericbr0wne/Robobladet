dfd4f277bee0ad4b6b862d3bc44b9159128d035a
VValdo <v.sersam@gmail.com>
2024-08-20 11:40:42 +0200

    navbar component added and imported. refactored main -> app. css edits

--- roboApp/src/App.jsx
+++ roboApp/src/App.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Main from "./main";
+import Header from "./components/Header";
+import ArticleList from "./components/ArticleList";
 import Privacy from "./components/Privacy";
-import Robot from './components/Robot';
-
+import Robot from "./components/Robot";
+import Navbar from "./components/Navbar";
+import "./App.css";
 
 function App() {
   return (
     <Router>
+      <Header />
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <ArticleList />
+            </div>
+          }
+        />
         <Route path="/privacy" element={<Privacy />} />
       </Routes>
       <Robot />
     </Router>
-
   );
 }
 
---
+++ roboApp/src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="robo-nav">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/privacy">
+            Privacy
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/categories">
+            Categories
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
