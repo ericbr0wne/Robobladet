6e4a4c90979233064d6a5128824c4e9ccb53a1d7
matilda002 <matilda0817@hotmail.com>
2024-08-20 09:55:36 +0200

    reformatting and replace classes with id
    - reformat files / cleanup
    - instead of having wrongly defined classes, switched out to id

--- roboApp/src/components/PrivacySlideshow.jsx
+++ roboApp/src/components/PrivacySlideshow.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../components/PrivacySlideshow.css";
 
-// Img for the slideshow
+// Images for the slideshow
 import img1 from "../assets/slideshow-images/img-1.jpg";
 import img2 from "../assets/slideshow-images/img-2.jpg";
 import img3 from "../assets/slideshow-images/img-3.jpg";
@@ -11,40 +11,40 @@ import img6 from "../assets/slideshow-images/img-6.jpg";
 import img7 from "../assets/slideshow-images/img-7.jpg";
 
 const PrivacySlideshow = () => {
-    const [slideIndex, setSlideIndex] = useState(0);
+  const [slideIndex, setSlideIndex] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setSlideIndex((prevIndex) => (prevIndex + 1) % slides.length);
-        }, 3000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSlideIndex((prevIndex) => (prevIndex + 1) % slides.length);
+    }, 3000);
 
-        return () => clearInterval(interval); // Cleanup interval on unmount
-    });
+    return () => clearInterval(interval); // Cleanup interval on unmount
+  });
 
-    const slides = [img1, img2, img3, img4, img5, img6, img7];
+  const slides = [img1, img2, img3, img4, img5, img6, img7];
 
-    return (
-        <div id="slideshow-container">
-            {slides.map((slide, index) => (
-                <div
-                    key={index}
-                    className="slides"
-                    style={{ display: index === slideIndex ? "block" : "none" }}
-                >
-                    <img className="slideshow" src={slide} alt={`Slide ${index + 1}`} />
-                </div>
-            ))}
-            <br />
-            <div id="bullets">
-                {slides.map((_, index) => (
-                    <span
-                        key={index}
-                        className={`bullet ${index === slideIndex ? "active" : ""}`}
-                    ></span>
-                ))}
-            </div>
+  return (
+    <div id="slideshow-container">
+      {slides.map((slide, index) => (
+        <div
+          key={index}
+          id="slides"
+          style={{ display: index === slideIndex ? "block" : "none" }}
+        >
+          <img id="slideshow" src={slide} alt={`Slide ${index + 1}`} />
         </div>
-    );
+      ))}
+      <br />
+      <div id="bullets">
+        {slides.map((_, index) => (
+          <span
+            key={index}
+            className={`bullet ${index === slideIndex ? "active" : ""}`}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default PrivacySlideshow;
