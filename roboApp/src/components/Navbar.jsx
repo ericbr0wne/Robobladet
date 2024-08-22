3e4f9e9cd35d665c9c6644052697a6398269b6c7
matilda002 <matilda0817@hotmail.com>
2024-08-22 15:52:53 +0200

    removed register from navbar
    only login, it links to register

--- roboApp/src/components/Navbar.jsx
+++ roboApp/src/components/Navbar.jsx
@@ -31,13 +31,8 @@ const Navbar = () => {
           </Link>
         </li>
         <li className="nav-item">
-          <Link className="nav-link" to="/register">
-            Register
-          </Link>
-        </li>
-        <li className="nav-item">
           <Link className="nav-link" to="/login">
-            Login
+            Logga in
           </Link>
         </li>
       </ul>
