17f77c70b2630f4afe0e68fe47e4d81f9d4c78cb
Eric Browne <114211022+ericbr0wne@users.noreply.github.com>
2024-08-26 08:09:07 +0200

    Update Privacy.jsx

--- roboApp/src/components/Privacy.jsx
+++ roboApp/src/components/Privacy.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import ContactForm from "../components/ContactForm";
 import PrivacySlideshow from "../components/PrivacySlideshow";
+import "../components/Privacy.css";
 import { Link } from "react-router-dom";
 
 const Privacy = () => {
