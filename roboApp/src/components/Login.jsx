fb78f6dc13eb5ff4597ae6f7ccda2ad20b4cb083
matilda002 <matilda0817@hotmail.com>
2024-08-26 09:23:24 +0200

    changed header to swe
    instead of login it is logga in

--- roboApp/src/components/Login.jsx
+++ roboApp/src/components/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
 
   return (
     <div>
-      <h1>Login</h1>
+      <h1>Logga in</h1>
       <div className="regLoginContainer">
         <div className="regLoginForm">
           <div>
